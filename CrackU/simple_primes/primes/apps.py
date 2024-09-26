d91c35a074ee9de42f792b29a43c8d44684197bf
Shreyash <shreyashsherigar1000@gmail.com>
2024-09-26 19:24:51 +0530

    Delete CrackU/simple_primes directory

--- CrackU/simple_primes/primes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrimesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'primes'
--- CrackU/simple_primes/primes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.input.as_view(),name='input'),
-    path('primes', views.output.as_view(),name='output'),
-]
